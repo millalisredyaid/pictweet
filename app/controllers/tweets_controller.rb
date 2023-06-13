292298cc995367cf3f90c652ee3536cef75ec66d
millalisredyaid <milanaxrazdukhova@gmail.com>
2023-06-13 21:26:29 +0900

    エラーメッセージ表示

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,65 @@
+class TweetsController < ApplicationController
+  before_action :set_tweet, only: [:edit, :show]
+  before_action :move_to_index, except: [:index, :show, :search]
+
+  def index
+    @tweets = Tweet.includes(:user).order("created_at DESC")
+  end
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    
+    @tweet = Tweet.new(tweet_params)
+    #バリデーションに引っかからず保存されれば、「投稿完了」の画面が呼び出される
+    if @tweet.save
+    else
+      #バリデーションに引っかかり保存されなければ、「新規投稿」の画面を呼び出す
+      render 'new'
+    end
+  end
+
+  def destroy
+    tweet = Tweet.find(params[:id])
+    tweet.destroy
+  end
+
+  def edit
+  end
+
+  def update
+    @tweet = Tweet.find(params[:id])
+    #バリデーションに引っかからず更新されれば、「更新完了」の画面が呼び出される
+    if @tweet.update(tweet_params)
+    else
+      #バリデーションに引っかかり保存されなければ、「編集」の画面が呼び出される
+      render 'edit'
+    end
+  end
+
+  def show
+    @comment = Comment.new
+    @comments = @tweet.comments.includes(:user)
+  end
+
+  def search
+    @tweets = Tweet.search(params[:keyword])
+  end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:image, :text).merge(user_id: current_user.id)
+  end
+
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
+end
---
+++ app/models/tweet.rb
@@ -0,0 +1,17 @@
+class Tweet < ApplicationRecord
+  validates :text, presence: true
+  belongs_to :user
+  has_many :comments
+
+  # imageも空で投稿できないように追記
+  validates :text, :image, presence: true
+
+  def self.search(search)
+    if search != ""
+      Tweet.where('text LIKE(?)', "%#{search}%")
+    else
+      Tweet.all
+    end
+  end
+end
+
