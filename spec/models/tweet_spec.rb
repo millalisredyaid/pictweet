5c0e4a4d2338d17b6c89ddef2e44a8bb4296389a
millalisredyaid <milanaxrazdukhova@gmail.com>
2023-06-19 23:38:11 +0900

    テストコード日本語化

--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -8,23 +8,30 @@ RSpec.describe Tweet, type: :model do
   describe 'ツイートの保存' do
     context 'ツイートが投稿できる場合' do
       it '画像とテキストを投稿できる' do
-        expect(@tweet).to be_valid
+        expect(@tweet.errors.full_messages).to be_empty
       end
-      it 'テキストのみで投稿できる' do
-        @tweet.image = ''
-        expect(@tweet).to be_valid
-      end
+      # it 'テキストのみで投稿できる' do
+      #   @tweet.text = '犠牲指紋右利きむこう。'
+      #   @tweet.valid?
+      #   expect(@tweet.errors.full_messages).not_to include('画像を入力してください')
+      # end
+      
     end
     context 'ツイートが投稿できない場合' do
       it 'テキストが空では投稿できない' do
         @tweet.text = ''
         @tweet.valid?
-        expect(@tweet.errors.full_messages).to include("Text can't be blank")
-      end     
+        expect(@tweet.errors.full_messages).to include('テキストを入力してください')
+      end 
+      it '画像がないとツイートは保存できない' do
+        @tweet.image = nil
+        @tweet.valid?
+        expect(@tweet.errors.full_messages).to include('画像を入力してください')
+      end    
       it 'ユーザーが紐付いていなければ投稿できない' do
         @tweet.user = nil
         @tweet.valid?
-        expect(@tweet.errors.full_messages).to include('User must exist')
+        expect(@tweet.errors.full_messages).to include('Userを入力してください')
       end
     end
   end
